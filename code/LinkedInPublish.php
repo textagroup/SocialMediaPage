ef2079627c3cf1fe3a6a55e51c595be9d905b5a3
Kirk Mayo <kirk@stripetheweb.com>
2016-04-07 15:10:00 +1200

    Setting up Social Media Page

---
+++ code/LinkedInPublish.php
@@ -0,0 +1,38 @@
+<?php
+
+class LinkedinPublish extends Controller {
+    public function postMessage($message) {
+        //Share on linkedIn
+        $siteConfig = SiteConfig::current_site_config();
+
+        $linkedIn = new Happyr\LinkedIn\LinkedIn(
+            $siteConfig->LinkedInId,
+            $siteConfig->LinkedInSecret
+        );
+        $accessToken = $siteConfig->LinkedInAccessToken;
+
+        $linkedIn->setAccessToken($accessToken);
+
+        if ($linkedIn->isAuthenticated()) {
+            //we know that the user is authenticated now. Start query the API
+            $options = array(
+                'json' => array(
+                    'comment' => $message,
+                    'visibility' => array(
+                        'code' => 'anyone'
+                    )
+                )
+            );
+            $result = $linkedIn->post('v1/people/~/shares', $options);
+            if ($linkedIn->hasError()) {
+                return $linkedIn->hasError();
+            }
+            return true;
+        } else {
+            //if not authenticated
+            $url = $linkedIn->getLoginUrl();
+            // TODO Internationalisation
+            return "<a href='$url'>Login with LinkedIn</a>";
+        }
+    }
+} 
